a870322e7fc9c06026b1d450503e3636509bc398
rookie-ysj <56711831+rookie-ysj@users.noreply.github.com>
2024-09-16 01:45:52 +0000

    Deploying to github-pages from @ rookie-ysj/some-vue-component@97b5f6bf5fb39abee9096f89365df06fe83f72a2 🚀

---
+++ assets/Cyberpunk.stories-BIhuWsLh.js
@@ -0,0 +1,23 @@
+import{d as h,b as s,o as l,e as u,F as k,f as x,n as p,t as v}from"./vue.esm-bundler-DksWrH1k.js";function i(e){return typeof e=="number"?e+"px":e}const y=h({__name:"Cyberpunk",props:{text:{default:"Cyberpunk"},height:{default:200},width:{default:"100%"},fontSize:{default:80},fontStyle:{default:"normal"}},setup(e){const t=e,n=s(()=>i(t.fontSize)),r=s(()=>i(t.width)),o=s(()=>i(t.height));return(m,w)=>(l(),u("svg",{style:p({width:r.value,height:o.value}),class:"ysj-cyberpunk"},[(l(!0),u(k,null,x(Array.from({length:5}),(z,b)=>(l(),u("text",{key:b,style:p({fontSize:n.value,fontStyle:m.fontStyle}),class:"cyberpunk-text","dominant-baseline":"middle","text-anchor":"middle",x:"50%",y:"50%"},v(m.text),5))),128))],4))}}),S=(e,t)=>{const n=e.__vccOpts||e;for(const[r,o]of t)n[r]=o;return n},_=S(y,[["__scopeId","data-v-38a7f637"]]);y.__docgenInfo={exportName:"default",displayName:"Cyberpunk",description:"",tags:{},props:[{name:"text",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"'Cyberpunk'"}},{name:"height",required:!1,type:{name:"union",elements:[{name:"number"},{name:"string"}]},defaultValue:{func:!1,value:"200"}},{name:"width",required:!1,type:{name:"union",elements:[{name:"number"},{name:"string"}]},defaultValue:{func:!1,value:"'100%'"}},{name:"fontSize",required:!1,type:{name:"union",elements:[{name:"number"},{name:"string"}]},defaultValue:{func:!1,value:"80"}},{name:"fontStyle",required:!1,type:{name:"union",elements:[{name:'"normal"'},{name:'"italic"'}]},defaultValue:{func:!1,value:"'normal'"}}],sourceFiles:["/home/runner/work/some-vue-component/some-vue-component/packages/components/Cyberpunk/Cyberpunk.vue"]};function C(e){return e.install=t=>{t.component(e.name,e)},e}const g=C(_),V={title:"Example/Cyberpunk",component:g,tags:["autodocs"],argTypes:{text:{control:{type:"text"}},width:{control:{type:"text"}},height:{control:{type:"text"}},fontStyle:{control:{type:"select"},options:["normal","italic"]},fontSize:{control:{type:"number"}}}},a={args:{text:"Storybook",fontStyle:"italic"},render:e=>({components:{Cyberpunk:g},setup(){return{args:e}},template:`
+      <div style="width: 900px">
+        <Cyberpunk v-bind="args"/>
+      </div>`})};var c,f,d;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    text: "Storybook",
+    fontStyle: "italic"
+  },
+  render: args => ({
+    components: {
+      Cyberpunk
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+      <div style="width: 900px">
+        <Cyberpunk v-bind="args"/>
+      </div>\`
+  })
+}`,...(d=(f=a.parameters)==null?void 0:f.docs)==null?void 0:d.source}}};const D=["Default"];export{a as Default,D as __namedExportsOrder,V as default};
